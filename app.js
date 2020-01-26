b63232b365ff5920561f328e121128a00a46c97f
Sidd R <siddphunk@gmail.com>
2020-01-26 14:13:30 +1100

    add gitignore, procfile

--- app.js
+++ app.js
@@ -149,9 +149,12 @@ app.post("/delete", function (req,res) {
      res.render("about");
  });
 
-
-app.listen (3000, function() {
-    console.log("Server is running on port 3000")
+let port = process.env.PORT;
+if(port == null || port =="") { 
+    port=3000;
+}
+app.listen (port, function() {
+    console.log("Server is running")
     
 
 }); 
